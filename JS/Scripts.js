9b759c9e95e5aa2dc95319c9c5e024b1f2903e52
shivamsngh <shivamsngh@hotmail.com>
2016-09-20 03:26:33 +0530

    using indexedDB

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -11,11 +11,38 @@ $(document).on('click','.close',function(){
 
 });
 
+
+
 function addTask(){
+    if (!window.indexedDB) {
+        window.alert("Your browser doesn't support a stable version of IndexedDB.")
+    }
+    else {
+        /*var subject = $("#newTaskSubject").val();
+        var desc = $("#newTaskDesc").val();    
+        var jsonData = {Subject: subject, Description: desc};
+        localStorage.setItem('taskList', JSON.stringify(jsonData));*/
+        window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+        window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+        window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
     var subject = $("#newTaskSubject").val();
-    var desc = $("#newTaskDesc").val();    
-    var jsonData = {Subject: subject, Description: desc};
-    localStorage.setItem('taskList', JSON.stringify(jsonData));
+        var desc = $("#newTaskDesc").val(); 
+    var tdData=[{}];
+       
+    var request = db.transaction(["tdData"], "readwrite")
+   .objectStore("tdData")
+   .add({ subject: subject, description: desc});
+   
+   request.onsuccess = function(event) {
+      alert("New task "+subject+"  has been created.");
+   };
+   
+   request.onerror = function(event) {
+      alert("Unable to add data\r\nIt already exist in your database! ");
+   }
+}
+    
+
     showTask();
 }
 
@@ -25,9 +52,7 @@ function showTask(){
 }
 
 $(document).on('click','#list',function(){
-    
-
-  $(".hide").css("visibility","visible");
+$(".hide").css("visibility","visible");
 
 });
 
